6754fb6973b60aac41fee27f5eee4b350b506f24
LennyFaceCoin <arii.sichoix@gmail.com>
2022-03-16 10:55:36 -0400

    creation of the CICD pipeline

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="novalabs",
-    version="0.0.6",
+    version="0.0.8",
     author="Nova Labs",
     author_email="devteam@novalabs.ai",
-    description="Nova API & Exchange client",
+    description="Nova Labs aims to facilitate the development of algorithmic trading and this package help to backtest and productionize technical strategies on Crypto Market",
     url="https://github.com/Nova-DevTeam/nova-python",
     packages=setuptools.find_packages(),
     classifiers=[
