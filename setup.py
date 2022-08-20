38e4c6e743855690de0c383a4c59c8d77e832cbb
Maël LE PETIT <makibeardy@gmail.com>
2022-08-20 18:29:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setuptools.setup(
     version="0.5.89",
     author="Nova Labs",
     author_email="devteam@novalabs.ai",
-    description="Nova Labs aims to facilitate the development of algorithmic trading and this package help to backtest and productionize technical strategies on Crypto Market",
+    description="Wrappers around Nova Labs code utilities focused on safety and testability",
+    long_description=long_description,
     url="https://github.com/Nova-DevTeam/nova-python",
     packages=setuptools.find_packages(),
     classifiers=[
