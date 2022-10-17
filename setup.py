ae668f24f51a6b9ae4b9bdd7adf07cb7125d9813
Arii Sichoix <arii.sichoix@gmail.com>
2022-10-16 23:00:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="novalabs",
-    version="1.2.03",
+    version="1.2.1",
     author="Nova Labs",
     author_email="devteam@novalabs.ai",
     description="Wrappers around Nova Labs utilities focused on safety and testability",
