0afe7c17296e611312217a5177473ab39ea1fdf0
bryansx <46893530+bryansx@users.noreply.github.com>
2022-11-21 12:18:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="novalabs",
-    version="1.2.73",
+    version="1.2.74",
     author="Nova Labs",
     author_email="devteam@novalabs.ai",
     description="Wrappers around Nova Labs utilities focused on safety and testability",
